b17f21e217a20a768f284882dd9e5f48bd7e5a9d
unknown <mwernerowicz@gmail.com>
2018-01-23 21:42:50 +0100

    init

---
+++ js/svgAnim.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+console.log('animating!');
+        new Vivus('logosvg', {
+            
+            duration: 200,
+            start: 'autostart',
+            type: 'sync',
+            animTimingFunction: Vivus.EASE,
+            reverseStack:'false'
+
+        }, function() {
+         
+            $('#logosvg path').attr('stroke-width','0')
+            $('#logosvg path').attr('fill','black')
+          
+            
+
+        });
+
+});
